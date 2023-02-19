56e8dabecc76ec274dd31605579fff2258e031b3
ZeroKPunk <jusonxia@gmail.com>
2023-02-19 22:52:51 +0800

    update account gadget

--- zkmpt-circuit/src/gadgets/account.rs
+++ zkmpt-circuit/src/gadgets/account.rs
@@ -1,11 +1,14 @@
 use halo2_proofs::{
-    circuit::Chip,
+    circuit::{Chip, Region, Value},
     halo2curves::FieldExt,
-    plonk::{Advice, Column, ConstraintSystem, Selector},
+    plonk::{Advice, Column, ConstraintSystem, Selector, Error},
     poly::Rotation,
 };
 
 use crate::{operation::Account, state_trie::HashTable};
+
+pub const CIRCUIT_ROW: usize = 4;
+const LAST_ROW: usize = CIRCUIT_ROW - 1;
 
 #[derive(Clone, Debug)]
 pub(crate) struct AccountGadget {}
@@ -21,6 +24,7 @@ struct AccountChipConfig {
 }
 
 struct AccountChip<'d, F> {
+    offset: usize,
     config: &'d AccountChipConfig,
     data: &'d Account<F>,
 }
@@ -79,6 +83,20 @@ impl<'d, Fp: FieldExt> AccountChip<'d, Fp> {
             hash_table.build_lookup(meta, enable, fst, snd, hash)
         });
 
+        // third hash lookup (Poseidon(nonce, balance) = hash3)
+        meta.lookup_any("account hash3 calc", |meta| {
+            // only enable on row 1
+            let s_enable = meta.query_advice(s_enable, Rotation::cur());
+            let enable_rows = meta.query_advice(s_ctrl_type[1], Rotation::cur());
+            let enable = enable_rows * s_enable;
+
+            let fst = meta.query_advice(acc_data_fields, Rotation::prev());
+            let snd = meta.query_advice(acc_data_fields, Rotation::cur());
+            let hash = meta.query_advice(intermediate_1, Rotation::cur());
+
+            hash_table.build_lookup(meta, enable, fst, snd, hash)
+        });
+
         // equality constraint: hash_final and Root
         meta.create_gate("account calc equalities", |meta| {
             let s_enable = meta.query_selector(sel) * meta.query_advice(s_enable, Rotation::cur());
@@ -104,5 +122,66 @@ impl<'d, Fp: FieldExt> AccountChip<'d, Fp> {
         }
     }
 
-    fn assign() {}
+    fn assign(&self, region: &mut Region<'_, Fp>) -> Result<usize, Error> {
+        let config = self.config();
+        let data = self.loaded();
+        // fill the connected circuit
+        let offset = self.offset - 1;
+        region.assign_advice(
+            || "account hash final", 
+            config.acc_data_fields, 
+            offset, 
+            || Value::known(data.account_hash()),
+        )?;
+
+        // fill the main block of chip
+        for (col, vals, desc) in [
+            (
+                config.acc_data_fields,
+                [data.nonce, data.gas_balance, data.tx_hash.0],
+                "data field",
+            ),
+            (
+                config.acc_data_fields_ext,
+                [Fp::zero(), Fp::zero(), data.tx_hash.1],
+                "data field ext",
+            ),
+            (
+                config.intermediate_2,
+                [data.account_hash(), data.hash_traces(1), data.state_root],
+                "intermediate 2",
+            ),
+            (
+                config.intermediate_1,
+                [Fp::zero(), data.hash_traces(2), data.hash_traces(0)],
+                "intermediate 1",
+            )
+        ] {
+            for (i, val) in vals.iter().enumerate() {
+                region.assign_advice(
+                    || format!("{} row {} (offset {})", desc, i, self.offset),
+                    col,
+                    self.offset + i,
+                    || Value::known(*val),
+                )?;
+            }
+         }
+
+        // row 4: notice this is not belong to account chip in general
+        region.assign_advice(
+            || "state root",
+            config.acc_data_fields,
+            self.offset + LAST_ROW,
+            || Value::known(self.data.state_root),
+        )?;
+
+        region.assign_advice(
+            || "state root padding",
+            config.acc_data_fields_ext,
+            self.offset + LAST_ROW,
+            || Value::known(Fp::zero()),
+        )?;
+
+        Ok(self.offset + LAST_ROW)
+    }
 }
